9fb1e6288c4345b74ef35c3d3f155199e10aa653
Sean Feil <sean@tophatandmonocle.com>
2017-04-18 22:18:17 -0600

    Add console

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
+	console.log("Serving request!");
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Azure!?");
+
 
 });
 
