096146a29b35a51845b11e8c3a4148b7228c63bc
Sean Feil <sean@tophatandmonocle.com>
2017-04-18 22:38:53 -0600

    More change

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
+
 
 http.listen(process.env.PORT, function(){
   console.log('listening on *:' + process.env.PORT);
