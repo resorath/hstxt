f3833a3d6871ab4af1ccffef2dabdd34188627f6
Sean Feil <sean@tophatandmonocle.com>
2017-05-02 23:22:56 -0600

    Flush out data structures

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,10 +14,12 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('command', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
-    setTimeout(function() { io.emit('chat message', "followup")}, 1000);
+    
+    parseCommand(msg);
+
+    setTimeout(function() { io.emit('control', "enemyturn")}, 1000);
   });
 
 
@@ -24,7 +27,75 @@ io.on('connection', function(socket){
 
 var port = process.env.PORT || 8000;
 
+var hand = {
+  p1: {
+
+  },
+  p2: {
+
+  }
+};
+
+var board = {
+  p1: {
+
+  },
+  p2: {
+
+  }
+};
+
+var deck = {
+  p1: {
+
+  },
+  p2: {
+
+  }
+}
+
+var cards = JSON.parse(fs.readFileSync("cards.json"));
+
+console.log(getCardByName("Doomsayer"));
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
 
+function parseCommand(command)
+{
+  if(!command)
+    return null;
+
+  var parts = command.split(" ");
+  var root = parts.shift();
+
+  if(typeof cfunc[root] === 'function')
+    cfunc[root](parts)
+
+}
+
+function getCardByName(name)
+{
+  var returnVal = null;
+
+  cards.forEach(function(card)
+  {
+    if(card["name"] && card["name"].toUpperCase() === name.toUpperCase())
+    {
+      returnVal = card;
+      return;
+    }
+  })
+
+  return returnVal;
+}
+
+// command functions
+var cfunc = { };
+
+cfunc.meow = function(parts)
+{
+  console.log("mew mew");
+  console.log(parts);
+}
